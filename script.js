5df1db120119ea83134cd5c6a32c4e07ec330186
Mathéo Dumont <math.dumont.md@gmail.com>
2018-03-28 16:32:16 +0200

    push branche

--- script.js
+++ script.js
@@ -1,24 +1,49 @@
-var url = 'https://www.numerama.com/feed/';
+window.addEventListener("load",function(){
 
 
 
 
+    var buttonURL = document.getElementById("buttonURL");
+    var inputURL = document.getElementById("inputURL");
+    console.log(inputURL);
+    console.log(buttonURL);
 
-
-
-
-
-
-
-
-async function ajax(url)
-{
-    let result = await fetch(url, 
-    {
-        body :jsonObjet,
-        headers:{
-            'content-type': 'application/xml',
-        },
-        method : 'GET',
+    buttonURL.addEventListener("click",ajax);
+    buttonURL.addEventListener("click",()=>{
+        console.log("fired"),
     });
-}
+
+
+
+
+
+
+
+
+
+
+    async function ajax()
+    {
+        let url = inputURL.value;
+        console.log(url);
+        let result = await fetch(url, 
+        {
+            body :jsonObjet,
+            headers:{
+                'content-type': 'application/xml',
+            },
+            method : 'GET',
+        });
+
+        if(result.status == 404)
+        {
+            alert("L'url donnée n'éxiste pas ! ");
+        }
+        else
+        {        
+            parser = new DOMParser();
+            xmlDoc = parser.parseFromString(response.text(),"text/xml");
+            console.log(xmlDoc);
+        }
+    }
+});
