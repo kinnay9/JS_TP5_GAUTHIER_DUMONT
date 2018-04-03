5d9be1d3bb324de83169b12f4b3e1ad978a02436
yannick gauthier <yannick.gauthier.game@hotmail.fr>
2018-04-03 08:10:24 +0200

    petites avancées

--- scriptTD.js
+++ scriptTD.js
@@ -1,139 +1,52 @@
 window.addEventListener("load",function() {
 
-    var canvas = document.getElementById('mon_canvas');
-        if(!canvas)
-        {
-            alert("Impossible de récupérer le canvas");
-            return;
-        }
-
-    var context = canvas.getContext('2d');
-        if(!context)
-        {
-            alert("Impossible de récupérer le context du canvas");
-            return;
-        }
-
-    var mode = "line";
-    var modeColor = false;
-    var xDebut,yDebut,xfin,yfin;
-    var colorFill = '#ac41f4';
-
-    document.getElementById('mon_canvas').addEventListener("mousedown",debut);
-    document.getElementById('mon_canvas').addEventListener("mouseup",fin);
-    document.getElementById('rect').addEventListener("click",modeRect);
-    document.getElementById('circle').addEventListener("click",modeCircle);
-    document.getElementById('line').addEventListener("click",modeLine);
-    document.getElementById('clear').addEventListener("click",clean);
-    document.getElementById('color').addEventListener("click",color);
-    document.getElementById('bleu').addEventListener("click",bleu);
-    document.getElementById('rouge').addEventListener("click",rouge);
-    document.getElementById('vert').addEventListener("click",vert);
+	setTime()
+	setTotalTime();
 
 
+	var videoPlay = false
+	document.getElementById('play').addEventListener('click',playPause)
+	document.getElementById('volume').addEventListener('change',changeVolume)
+	document.getElementById('time').addEventListener('change',changeTime)
 
+	function playPause(){
+		if (videoPlay) {
+			document.getElementById('video').pause()
+			videoPlay = false
+		}else{
+			document.getElementById('video').play()
+			videoPlay = true
+		}
+	}
 
+	function changeVolume(){
+		document.getElementById('video').volume = (document.getElementById('volume').value)/100
+	}
 
-    function debut(e){
-        if (mode == "line") {
-            context.beginPath();
-            context.lineTo(e.pageX, e.pageY);
-        }else if (mode == "rect") {
-            xDebut = e.pageX;
-            yDebut = e.pageY;
-        }else if (mode == "circle") {
-            context.beginPath(); 
-            xDebut = e.pageX;
-            yDebut = e.pageY;
-        }
-    }
+	function setTime(){
+		function actualiser(){
+			var str = ""
+			var currentTime =  document.getElementById('video').currentTime
+			var seconde = currentTime%60 - (currentTime%60)%1
+			var minute = currentTime/60 - (currentTime/60)%1
+			str = minute + " : " + seconde
+			var tempspourcent = (currentTime*100)/document.getElementById('video').duration
+			document.getElementById("time").value = tempspourcent - tempspourcent%1
+			document.getElementById("currentTime").innerHTML = str;
+		}
+		setInterval(actualiser,1000);
+	}
 
-    function fin(e){
-        if (!modeColor) {
-            if (mode == "line") {
-                context.lineTo(e.pageX, e.pageY);
-                context.stroke();
-                context.closePath();
-            }else if (mode == "rect") {
-                xfin = e.pageX;
-                yfin = e.pageY;
-                context.strokeRect(xDebut, yDebut, xfin - xDebut, yfin - yDebut);
+	function setTotalTime(){
+		var str = ""
+		var totalTime =  document.getElementById('video').duration
+		var seconde = totalTime%60 - (totalTime%60)%1
+		var minute = totalTime/60 - (totalTime/60)%1
+		str = minute + " : " + seconde
+		document.getElementById("totalTime").innerHTML = str;
+	}
 
-            }else if(mode == "circle") {
-                xfin = e.pageX;
-                yfin = e.pageY;
-                var rayonX =Math.abs(xfin - xDebut);
-                var rayonY =Math.abs(yfin - yDebut);
-                if (rayonY > rayonX) {
-                    context.arc(xDebut, yDebut, rayonY, 0, Math.PI*2); 
-                }else {
-                    context.arc(xDebut, yDebut, rayonX, 0, Math.PI*2); 
-                }
-                context.stroke();
-                context.closePath();
-            }
-        }else{
-            context.fillStyle = colorFill;
-            if (mode == "line") {
-                context.lineTo(e.pageX, e.pageY);
-                context.fill();
-                context.closePath();
-            }else if (mode == "rect") {
-                xfin = e.pageX;
-                yfin = e.pageY;
-                context.fillRect(xDebut, yDebut, xfin - xDebut, yfin - yDebut);
-
-            }else if(mode == "circle") {
-                xfin = e.pageX;
-                yfin = e.pageY;
-                var rayonX =Math.abs(xfin - xDebut);
-                var rayonY =Math.abs(yfin - yDebut);
-                if (rayonY > rayonX) {
-                    context.arc(xDebut, yDebut, rayonY, 0, Math.PI*2); 
-                }else {
-                    context.arc(xDebut, yDebut, rayonX, 0, Math.PI*2); 
-                }
-                context.fill();
-                context.closePath();
-            }
-        }
-    }
-
-    function modeRect(){
-        mode = "rect";
-    }
-
-    function modeLine(){
-        mode = "line";
-    }
-
-    function modeCircle(){
-        mode = "circle";
-    }
-
-    function clean(){
-        context.fillStyle = 'rgb(255,255,255)';
-        context.fillRect(0,0,500,500);
-    }
-
-    function color(){
-        if (modeColor) {
-            modeColor = false;
-        }else{
-            modeColor = true;
-        }
-    }
-
-    function bleu(){
-        colorFill = '#24477f';
-    }
-
-    function rouge(){
-        colorFill = '#cc2222';
-    }
-
-    function vert(){
-        colorFill = '#4bf442';
-    }
-
+	function changeTime(){
+		document.getElementById('video').currentTime = ((document.getElementById('time').value)*document.getElementById('video').duration)/100
+	}
 });
